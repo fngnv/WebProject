eddb078e943ac635021d75a7f86ffa60c4a7158a
Vera <finogenova.vera4@gmail.com>
2022-09-29 22:10:16 +0300

    Lisätty toimiva laskuri

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -9,25 +9,26 @@ const amountInput = document.getElementById('gram');
 const priceInput = document.getElementById('price');
 const calcButton = document.getElementById('calculate');
 const mainTag = document.querySelector('main');
-let priceForGram
+let priceForMGram;
 
 calcButton.addEventListener('click', function () {
     let errorMsg = document.getElementById('error');
+    console.log("TYT");
     if(amountInput.value == "" || priceInput.value == "") {
-        errorMsg.innerText = 'Täytä kaikki kentät!'
-        //alert('Täytä kaikki kentät!');
+        //errorMsg.innerText = 'Täytä kaikki kentät!'
+        alert('Täytä kaikki kentät!');
     } else {
         let amountNumb = parseFloat(amountInput.value);
         let priceNumb = parseFloat(priceInput.value);
         if (amountNumb <= 0 || priceNumb <= 0) {
-            errorMsg.innerText = 'Laitathan kenttiin vain positiiviset luvut!';
-        } else {
-            priceForGram = (priceNumb / amountNumb).toFixed(2);
+            alert('Laitathan kenttiin vain positiiviset luvut!');
+         } else {
+            priceForMGram = (priceNumb / amountNumb).toFixed(2);
 
             mainTag.innerHTML = `
                 Yhden gramman hinta:
                 <br>
-                ${priceForGram} €
+                ${priceForMGram} €
             `;
         }
     }
