11193b2cedb6755efd9e77fb78a06579704c8a2d
suokiiski <104062080+suokiiski@users.noreply.github.com>
2022-10-05 20:51:57 +0300

    kommentointia tehty

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,10 +1,59 @@
-const post= document.getElementById("comment-button");
-post.addEventListener("click", function(){
-    let commentBoxValue= document.getElementById("comment-box").value;
+function supportsLocalStorage () {
+    return typeof localStorage !== 'undefined';
+}
 
-    let li = document.createElement("li");
-    let text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
+function haeKommentit() {
+    return JSON.parse(localStorage.getItem('comments')) || [];
+}
 
-});
+function tallennaKommentit (comments, commentStr, action) {
+    if (!commentStr && comments.indexOf(commentStr) < 0) {
+        action(err);
+    }
+    action(undefined, commentStr);
+}
+
+function appendToStream(stream, str, index) {
+    var li = document.createElement('LI');
+    li.setAttribute('data-index', index);
+    li.innerHTML = str;
+    stream.appendChild(li);
+}
+
+function loadComments(stream) {
+    var comments = haeKommentit();
+    if (comments) {
+        for (var i = 0; i < comments.length; i++) {
+            appendToStream(stream, comments[i], i);
+        }
+    }
+}
+
+supportsLocalStorage(); {
+    initApp();}
+
+function initApp() {
+
+    var commentForm = document.getElementById('kommentointi'),
+        commentList = document.getElementById('kommentit'),
+        commentInput = document.getElementById('kirjoita-tahan')
+
+    loadComments(commentList);
+
+    commentForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var commStr = commentInput.value,
+            comments = haeKommentit();
+
+        tallennaKommentit(comments, commStr, function(err, value) {
+            if (err) {
+                return;
+            }
+
+            comments.push(value);
+            localStorage.setItem('comments', JSON.stringify(comments));
+            appendToStream(commentList, commStr);
+            commentInput.value = '';
+        });
+    }, true);
+}
