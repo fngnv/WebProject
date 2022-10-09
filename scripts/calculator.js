3f6b54328c5a3ba0549b2859599d6d38910b9b99
Vera <finogenova.vera4@gmail.com>
2022-10-09 19:58:23 +0300

    Lisätty sisältö calculator-sivulle

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -5,14 +5,13 @@
 * */
  //TODO: jos hinta on pienempi kuin x, kysytään juoman nimi, ja sit lisätään tietokantaan
 
-const amountInput = document.getElementById('gram');
-const priceInput = document.getElementById('price');
-const calcButton = document.getElementById('calculate');
-const mainTag = document.querySelector('main');
+const amountInput = document.getElementById('gram1');
+const priceInput = document.getElementById('price1');
+const calcButton = document.getElementById('calculate1');
+const resultField1 = document.getElementById('result1');
 let priceForMGram;
 
 calcButton.addEventListener('click', function () {
-    console.log("TYT");
     if(amountInput.value === "" || priceInput.value === "") {
         //errorMsg.innerText = 'Täytä kaikki kentät!'
         alert('Täytä kaikki kentät!');
@@ -24,7 +23,7 @@ calcButton.addEventListener('click', function () {
          } else {
             priceForMGram = (priceNumb / amountNumb).toFixed(2);
 
-            mainTag.innerHTML = `
+            resultField1.innerHTML = `
                 Yhden miligramman hinta:
                 <br>
                 ${priceForMGram} €
@@ -33,17 +32,33 @@ calcButton.addEventListener('click', function () {
     }
 })
 
-/*let text = `Tähän tulee teksti`;
-let htmlText = ``;
-let asideContent = document.getElementById('cheapestList');
-let pic = `TÄHÄN TULEE KUVA
-            <br>
-            ^______________^ <br><br>`
+const weightInput = document.getElementById('kg2');
+const calcButton2 = document.getElementById('calculate2');
+const resultField2 = document.getElementById('result2');
+let lethalDose;
+let cups;
 
-for (let i = 0; i < 5; i++) {
-    htmlText += ` ${text} <br> 
-                ${pic}`;
-}
+calcButton2.addEventListener('click', function () {
+    if(weightInput.value === '') {
+        alert('Täytä kaikki kentät!');
+    } else {
+        let weightNumb = parseFloat(weightInput.value);
 
-asideContent.innerHTML = htmlText;*/
+        if(weightNumb <= 0) {
+            alert('Laitathan vain positiiviset luvut!');
+        } else {
+            lethalDose = 150 * weightNumb;
+            cups = (lethalDose/160).toFixed(0);
+        }
+
+        let htmlText = `
+            Letaali kofeiiniannoksesi on ${lethalDose} mg kofeiinia <br><br>
+            Se on yhtä kuin noin ${cups}&#1645; tölkkiä Megistä <br><br>
+            &#1645;Tämä on vain suuntaa antava luku. Emme kuitenkaan suosittele ylittämään tätä rajaa
+        `;
+
+        resultField2.innerHTML = htmlText;
+    }
+
+})
 
